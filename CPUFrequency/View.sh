7cdcb972b8e6f599ee8b40cb686e98f478a95382
xlivans <xlivans@qq.com>
2020-01-26 00:38:40 +0800

    Update

--- CPUFrequency/View.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/desktop/
-curl -O https://raw.githubusercontent.com/daliansky/Lenovo-Air13-IWL-Hackintosh/master/CPUFrequency/freqVectorsEdit.sh
-chmod +x ~/desktop/freqVectorsEdit.sh
-~/desktop/freqVectorsEdit.sh
---
+++ CPUFriend/View.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/desktop/
+curl -O https://raw.githubusercontent.com/Piker-Alpha/freqVectorsEdit.sh/master/freqVectorsEdit.sh
+chmod +x ~/desktop/freqVectorsEdit.sh
+~/desktop/freqVectorsEdit.sh
