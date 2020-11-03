50959e9d120198876b5fbd36039bc1907ea95e5a
xlivans <xlivans@qq.com>
2020-11-03 22:02:13 +0800

    Update ALCFixPlug and alc-verb

--- ALCPlugFix/Remove.sh
+++ ALCPlugFix/Remove.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo rm /usr/local/bin/ALCPlugFix
-sudo rm /usr/local/bin/hda-verb
+sudo rm /usr/local/bin/alc-verb
 sudo launchctl unload -w /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo launchctl remove /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo rm /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+echo Done !
--- ALCPlugFix/install.sh
+++ ALCPlugFix/install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 path=${0%/*}
+
+sudo mkdir /usr/local/bin
 sudo cp -f "$path/ALCPlugFix" /usr/local/bin
 sudo chmod 755 /usr/local/bin/ALCPlugFix
 sudo chown root:wheel /usr/local/bin/ALCPlugFix
-sudo cp -f "$path/hda-verb" /usr/local/bin
-sudo chmod 755 /usr/local/bin/hda-verb
-sudo chown root:wheel /usr/local/bin/hda-verb
-sudo cp -f "$path/good.win.ALCPlugFix.plist" /Library/LaunchDaemons/
+sudo cp -f "$path/alc-verb" /usr/local/bin
+sudo chmod 755 /usr/local/bin/alc-verb
+sudo chown root:wheel /usr/local/bin/alc-verb
+sudo cp -f "$path/good.win.ALCPlugFix.plist" /Library/LaunchDaemons
 sudo chmod 644 /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo chown root:wheel /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo launchctl load -w /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+echo Done !
