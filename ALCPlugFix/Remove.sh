31ba3547ebc78f98b667d994ba8fec832765a01a
xlivans <xlivans@qq.com>
2020-11-05 18:46:23 +0800

    Update ALCPlugFix-Swift 1.4

--- ALCPlugFix/Remove.sh
+++ ALCPlugFix/Remove.sh
@@ -5,6 +5,5 @@ sudo rm /Library/LaunchDaemons/com.black-dragon74.ALCPlugFix.plist
 sudo rm /usr/local/bin/ALCPlugFix
 sudo rm /usr/local/bin/alc-verb
 sudo rm -r /Library/Preferences/ALCPlugFix
-sudo rm -r /Library/Logs/ALCPlugFix
 
 echo Done !
--- ALCPlugFix/install.sh
+++ ALCPlugFix/install.sh
@@ -18,21 +18,20 @@ sudo rm -r /Library/Logs/ALCPlugFix
 
 sudo mkdir /usr/local/bin
 sudo mkdir /Library/Preferences/ALCPlugFix
-sudo mkdir /Library/Logs/ALCPlugFix
 
-sudo cp -f "$path/alc-verb" /usr/local/bin
+sudo cp "$path/alc-verb" /usr/local/bin
 sudo chmod 755 /usr/local/bin/alc-verb
 sudo chown root:wheel /usr/local/bin/alc-verb
 
-sudo cp -f "$path/ALCPlugFix" /usr/local/bin
+sudo cp "$path/ALCPlugFix" /usr/local/bin
 sudo chmod 755 /usr/local/bin/ALCPlugFix
 sudo chown root:wheel /usr/local/bin/ALCPlugFix
 
-sudo cp -f "$path/Config.plist" /Library/Preferences/ALCPlugFix
+sudo cp "$path/Config.plist" /Library/Preferences/ALCPlugFix
 sudo chmod 644 /Library/Preferences/ALCPlugFix/Config.plist
 sudo chown root:wheel /Library/Preferences/ALCPlugFix/Config.plist
 
-sudo cp -f "$path/com.black-dragon74.ALCPlugFix.plist" /Library/LaunchDaemons
+sudo cp "$path/com.black-dragon74.ALCPlugFix.plist" /Library/LaunchDaemons
 sudo chmod 644 /Library/LaunchDaemons/com.black-dragon74.ALCPlugFix.plist
 sudo chown root:wheel /Library/LaunchDaemons/com.black-dragon74.ALCPlugFix.plist
 sudo launchctl load /Library/LaunchDaemons/com.black-dragon74.ALCPlugFix.plist
